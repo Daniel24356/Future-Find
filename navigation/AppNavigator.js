38d8568306702cb1d531e77018ba2bc3d3899578
Tolulope Akinwande <tolulopeakinwande@Tolulopes-MacBook-Pro.local>
2025-02-26 15:12:16 +0100

    repay loan upadate

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import SplashScreen from '../screens/SplashScreen';
 import OnBoardingScreen from '../screens/OnBoardingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignUpScreen';
+import RepayLoan from '../screens/RepayLoan';
 
 const Stack = createStackNavigator();
 
@@ -11,10 +12,11 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Repayloan" component={RepayLoan} />
+      <Stack.Screen name="Splash" component={SplashScreen}/>
+      <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
