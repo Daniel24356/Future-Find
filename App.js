adc7ed1ab9472f1866c8b71f6fe8e88f21737fdc
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-27 15:30:09 +0100

    loan application

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-import AppNavigator from './navigation/AppNavigator';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+// import AppNavigator from './navigation/AppNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoanApplication from './screens/LoanApplication';
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  return <AppNavigator/>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Apply Loan" options = {{headerShown: false}}>
+          {() => <LoanApplication/>}
+          </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
--- screens/LoanApplication.js
+++ screens/LoanApplication.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 const LoanApplication = () => {
   return (
     <View>
-        <Text>Loan Applicationkor3d3
+      <View style = {styles.topDiv}>
+        <View style = {styles.overlay}>
+          <Image style = {styles.image} source={require('../assets/Coint.png')}/>
+          <Image style = {styles.image1} source={require('../assets/Coint.png')}/>
+          <Image style = {styles.image3} source={require('../assets/Coint.png')}/>
+        </View>
+      </View>
+        <Text>Loan Application
         </Text>
         <View></View>
         <View></View>
@@ -18,6 +25,35 @@ const styles = StyleSheet.create({
     container: {
        
     },
+
+    topDiv: {
+      height: 158,
+      // backgroundColor: "#442CF5"
+    },
+    overlay: {
+      height: 158,
+      backgroundColor: "#442CF5",
+      flex: 1,
+      flexDirection: "row",
+    },
+    image3: {
+      width: 118.88,
+      height:100,
+      top:58,
+      border: "solid, red",
+      left: 340
+    },
+    image: {
+      width: 118.88,
+      height:100,
+      top: 58,
+      left: -16
+    },
+    image1: {
+      width: 268.75,
+      height: 168.75,
+      opacity: 50
+    }
     
 
 })
