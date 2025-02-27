3481712f01a183a36ba38044b6c3657055482750
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-27 07:23:16 +0100

    loan application

---
+++ screens/LoanApplication.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const LoanApplication = () => {
+  return (
+    <View>
+        <Text>Loan Applicationkor3d3
+        </Text>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+       
+    },
+    
+
+})
+
+export default LoanApplication
