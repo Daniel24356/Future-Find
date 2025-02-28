083755d898135cd7045bbbe8b91d425014d38974
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-28 15:58:33 +0100

    Contribution Details -K

--- App.js
+++ App.js
@@ -2,14 +2,16 @@
 import AppNavigator from './navigation/AppNavigator';
 import Withdrawal from './screens/Withdrawal';
 import HomeScreen from './screens/HomeScreen';
+import ContributeDetails from './screens/ContributeDetails';
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name = "Apply Loan" options = {{headerShown: false}}>
+        {/* <Stack.Screen name = "Apply Loan" options = {{headerShown: false}}>
           {() => <LoanApplication/>}
-          </Stack.Screen>
+          </Stack.Screen> */}
+          <ContributeDetails/>
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ screens/ContributeDetails.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from "react-native"
+
+const ContributeDetails = () => {
+   return (
+     <View style = {styles.conContainer}>
+        <View style>
+
+        </View>
+     </View>
+   )
+}
+
+
+const styles = StyleSheet.create({
+     conContainer: {
+        backgroundColor: "#f5f7ff",
+     }
+})
+
+export default ContributeDetails
