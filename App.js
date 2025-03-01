71075b8c0753f8d226d31644aedbfaeb0db48f78
Korede Anunusor <koredeanunusor@gmai.com>
2025-03-01 18:58:47 +0100

    2nd Contibution details screen

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import HomeScreen from './screens/HomeScreen';
 import ContributeDetails from './screens/ContributeDetails';
 import { NavigationContainer } from '@react-navigation/native';
 import LoanApplication from './screens/LoanApplication';
+import ContributeScreen2 from './screens/ContributeScreen2';
 
 export default function App() {
   return (
     <NavigationContainer>
          {/* <LoanApplication/> */}
-          <ContributeDetails/>
+          {/* <ContributeDetails/> */}
+          <ContributeScreen2/>
     </NavigationContainer>
   )
 }
---
+++ screens/ContributeScreen2.js
@@ -0,0 +1,184 @@
+import { StyleSheet, View, Text, TextInput, Image} from "react-native"
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Custom2Button from "../props/Custom2Button";
+
+
+const ContributeScreen2 = () => {
+   return (
+     <View style = {styles.conContainer}>
+       <View style={styles.header}>
+                <View style={styles.innercont}>
+                    <View style={styles.smallBox}>
+                        <Ionicons name="chevron-back" size={20} color="black" />
+                    </View>
+                    <Text style={styles.create}>Create group</Text>
+                </View>
+            </View>
+
+             <View style = {styles.contribute}>
+               <View style = {styles.progress}>
+                  <View style = {[styles.bar, {backgroundColor: "#442CF5"}]}></View>
+                  <View style = {[styles.bar, {backgroundColor: "#dad5fd"}]}></View>
+               </View>
+              <View>
+              <Text style={styles.createT}>Create contribution group</Text>
+              <Text style = {styles.detail}>Enter your contribution details</Text>
+              </View>
+
+              <View style = {styles.form}>
+
+               <View style = {styles.inputDiv}>
+                  <View style = {styles.inDiv}>
+                    <Text style = {styles.inText}>Group name</Text>
+                  <TextInput style = {styles.input}
+                  placeholder= "Monthly savings"
+                  placeholderTextColor= "#6C727F"
+               ></TextInput>
+                  </View>
+               </View>
+
+               <View style = {styles.inputDiv}>
+                 <View style = {styles.inDiv}>
+                    <Text style = {styles.inText}>Amount per members</Text>
+                 <TextInput style = {styles.input}
+                  placeholder= "35,000"
+                  placeholderTextColor= "#6C727F"
+               ></TextInput>
+                 </View>
+               <Image style = {styles.inputImage} source={require('../assets/applyLoan/blackNaira.png')}/>
+               </View>
+
+               <View style = {styles.inputDiv}>
+              <View style = {styles.inDiv}>
+                <Text style = {styles.inText}>Payment interval</Text>
+              <TextInput style = {styles.input}
+                  placeholder= "Monthly"
+                  placeholderTextColor= "#6C727F"
+               ></TextInput>
+              </View>
+               <Image style = {styles.inputImage} source={require('../assets/applyLoan/arrowDown.png')}/>
+               </View>
+
+               <View style = {styles.inputDiv}>
+               <View style = {styles.inDiv}>
+                <Text style = {styles.inText}>Maximum members</Text>
+               <TextInput style = {styles.input}
+                  placeholder= "30"
+                  placeholderTextColor= "#6C727F"
+               ></TextInput>
+               </View>
+               </View>
+                  
+             </View>
+
+             </View>
+               <View style = {styles.conButton}>
+              <Custom2Button
+              backgroundColor = "#2C14DD"
+              title = "Continue"
+              />
+              </View>
+              
+     </View>
+   )
+}
+
+
+const styles = StyleSheet.create({
+     conContainer: {
+        backgroundColor: "#f5f7ff",
+     },
+     header: {
+      backgroundColor: "#442cf5",
+      height: 150,
+  },
+  smallBox: {
+      backgroundColor: 'blue', 
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 8,
+      backgroundColor: "white",
+      padding: 5
+  },
+  innercont: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      paddingLeft: 15,
+      gap: 10,
+      paddingTop: 90,
+  },
+  create: {
+      color: "white",
+      fontSize: 18
+  },
+  contribute:{
+      width: 392,
+      height: 450,
+      gap: 20,
+      marginTop: 40,
+      marginLeft: 17,
+      paddingLeft: 5,
+      paddingRight: 6
+  },
+  progress:{
+     flexDirection: "row",
+     gap: 20
+  },
+  bar: {
+    width: 180,
+    height: 3
+  },
+  createT: {
+      fontWeight: 700,
+      fontSize: 35
+  },
+  detail: {
+     fontSize: 18.5,
+     marginTop: 8
+  },
+  form: {
+   marginTop: 20,
+   gap: 20
+  },
+  inputDiv: {
+   height: 55,
+   flexDirection: "row",
+   backgroundColor: "#fff",
+   alignItems: "center",
+   borderRadius: 16,
+   paddingBottom: 8,
+   paddingRight: 12,
+   paddingLeft: 12,
+   paddingTop: 8,
+   gap: 10
+  },
+  inDiv: {
+    width: 325,
+    height: 45,
+    paddingTop: 5,
+    flexDirection: "column"
+  },
+  inText: {
+    fontSize: 15,
+    paddingLeft: 5,
+    color: "#8F94A3",
+    marginBottom: 2,
+  },
+  input: {
+   width: 325,
+   height: 30,
+   paddingTop: 2,
+   fontSize: 18,
+   fontWeight: 500,
+   color: "#292B2D"
+  },
+  conButton: {
+   width: 392,
+   marginLeft: 17,
+   marginTop: 170,
+   marginBottom: 55
+  }
+})
+
+export default ContributeScreen2
