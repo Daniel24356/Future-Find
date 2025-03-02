9a1d2f06aa94ece80501ba6a5ebc90d33aa90d8a
Daniel24356 <igwedaniel128@gmail.com>
2025-03-01 16:24:30 -0800

    main

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -12,12 +12,16 @@ import BetAccount from '../screens/BetAccount';
 import SignUpForm from '../screens/Signupform';
 import HomeScreen from '../screens/HomeScreen';
 import LoanLandingScreen from '../screens/LoanLandingScreen';
-import profileScreen from '../screens/profileScreen';
+import ProfileScreen from '../screens/profileScreen';
 import RepayLoan from '../screens/RepayLoan';
 import ResetPassword from '../screens/ResetPassword';
 import Settings from '../screens/Settings';
 import FixPlanScreen from '../screens/FixplanScreen';
 import FinalWithdrawal from '../screens/FinalWithdrawal';
+import AirtimeTopupScreen from '../screens/AirtimeTopupScreen'
+import Contribution_Active from '../screens/Contribution_Active';
+import Group_Details from '../screens/Group_Details';
+import Investment from '../screens/Investment';
 
 const Stack = createStackNavigator();
 
@@ -25,12 +29,7 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Setting" component={Settings} />
-        <Stack.Screen name="Resetpassword" component={ResetPassword} />
         <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="OnBoarding" component={OnBoardingScreen} />
-        <Stack.Screen name="TransactionDetails" component={TransactionDetails} />
-        <Stack.Screen name="BetAccount" component={BetAccount} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignUpForm} />
         <Stack.Screen name="onboarding1" component={OnboardingScreen1} />
@@ -38,10 +37,18 @@ const AppNavigator = () => {
         <Stack.Screen name="onboarding3" component={OnboardingScreen3} />
         <Stack.Screen name="home" component={HomeScreen} />
         <Stack.Screen name="loanLandingScreen" component={LoanLandingScreen} />
-        <Stack.Screen name="profile" component={profileScreen} />
+        <Stack.Screen name="profile" component={ProfileScreen} />
         <Stack.Screen name="repayLoan" component={RepayLoan} />
         <Stack.Screen name="fixPlan" component={FixPlanScreen} />
         <Stack.Screen name="finalwithdrawal" component={FinalWithdrawal} />
+        <Stack.Screen name="airtime" component={AirtimeTopupScreen} />
+        <Stack.Screen name="TransactionDetails" component={TransactionDetails} />
+        <Stack.Screen name="BetAccount" component={BetAccount} />
+        <Stack.Screen name="contributionActive" component={Contribution_Active} />
+        <Stack.Screen name="groupDetails" component={Group_Details} />
+        <Stack.Screen name="investment" component={Investment} />
+        <Stack.Screen name="Resetpassword" component={ResetPassword} />
+        <Stack.Screen name="Setting" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
