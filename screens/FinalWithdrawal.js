e8f934b10f23628ad917d6b8e7d03f7701d8cd9d
Uchenna99 <uchennaagbu@gmail.com>
2025-03-09 22:14:45 +0100

    final withdrawal page touched up

--- screens/FinalWithdrawal.js
+++ screens/FinalWithdrawal.js
@@ -7,118 +7,136 @@ import {
   Image, TextInput,
   ScrollView
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons"; 
 import TopHeader from "../props/TopHeader";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../props/CustomButton";
 
 const FinalWithdrawal =()=> {
+  const [activeButton, setActiveButton] = useState(false);
     return (
+      <View style={{flex:1,backgroundColor:'#F5F7FF'}}>
+        <SafeAreaView style={{flex:1}}>
+          <TopHeader title="Withdrawal" onRightPress={() => console.log("Settings Pressed")} />
 
-        <ScrollView style={styles.container}>
-          <SafeAreaView>
+          <ScrollView style={{flex:1}}>
         
-               <TopHeader title="Withdrawal" onRightPress={() => console.log("Settings Pressed")} />
+            <View style={{padding: 15,gap:30}}>
 
-               <View style={{padding: 10}}>
-                    <View style={styles.card}>
-                      <View style={styles.lockIcon}>
-                        <Image source={require("../assets/Upload.png")} style={{width: 30}}/>
-                      </View>
-                      <Text style={styles.totalInterest}>Total available amount</Text>
-                      <Text style={styles.amount}>N150,000</Text>
+              <View style={styles.card}>
 
-                      <View style={{flexDirection: "row", alignItems: "center", gap: 15}}>
-                      <View style={{backgroundColor: "#F5F7FF", flexDirection: "row", padding: 15, borderRadius: 10, gap: 10}}>
-                       <View style={styles.lock}>
-                          <Image source={require("../assets/Wallet.png")}/>
-                      </View>
-                      <View>
+                <View style={styles.lockIcon}>
+                  <Image source={require("../assets/Upload.png")} />
+                </View>
+
+                <View style={{alignItems:'center'}}>
+                  <Text style={styles.totalInterest}>Total available amount</Text>
+                  <Text style={styles.amount}>N150,000</Text>
+                </View>
+
+                <View style={{flexDirection: "row", gap: 10}}>
+                  <View style={styles.info}>
+                    <View style={styles.lock}>
+                        <Image source={require("../assets/Wallet.png")}/>
+                    </View>
+                    <View>
                       <Text style={styles.capital}>Your Capital</Text>
                       <Text style={styles.number}>N150,000</Text>
-                      </View>
-                       </View>
+                    </View>
+                  </View>
 
-                       <View style={{backgroundColor: "#F5F7FF", flexDirection: "row", padding: 15, borderRadius: 10, gap: 10}}>
-                       <View style={styles.locktwo}>
+                    <View style={styles.info}>
+                      <View style={styles.lock}>
                           <Image source={require("../assets/Dolls.png")}/>
                       </View>
                       <View>
-                      <Text style={styles.capital}>Total Interest</Text>
-                      <Text style={styles.number}>N50.00</Text>
+                        <Text style={styles.capital}>Total Interest</Text>
+                        <Text style={styles.number}>N50.00</Text>
                       </View>
-                       </View>
-                      </View>
-
-                      <View style={styles.warningBox}>
-                            <Image source={require("../assets/Danger.png")}/>
-                                <Text style={styles.warningText}>
-                                  You cannot withdraw your interest since your durations has not been completed
-                                </Text>
-                              </View>
-
                     </View>
-
-
-                    <View style={{flexDirection: "row", justifyContent: "space-between", marginTop: 25}}>
-                        <Text>Early Withdrawal Fee</Text>
-                        <Text style={{color: "red"}}>- N10.00</Text>
-                    </View>
-
-                     <View style={styles.passwordContainer}>
-                          <TextInput
-                            style={styles.passwordInput}
-                            placeholder="Amount"
-                            value=""
-                          />
-                           <Image source={require("../assets/NairaImg.png")}/>
-                        
-                        </View>
-
-                        <View style={{flexDirection: "row", alignItems: "center", justifyContent: "flex-end", gap: 20}}>
-                            <TouchableOpacity style={{backgroundColor: "white", borderColor: "#442CF5", borderWidth: 1, borderRadius: 10, padding: 5, paddingLeft: 24,  paddingRight: 24}}>
-                                <Text style={{color: "#442CF5"}}>All</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity style={{backgroundColor: "white", borderColor: "#442CF5", borderWidth: 1, borderRadius: 10, padding: 5, paddingLeft: 24,  paddingRight: 24}}>
-                                <Text style={{color: "#442CF5"}}>50%</Text>
-                            </TouchableOpacity>
-                        </View>
-
-                        <View style={styles.passwordContainer}>
-                          <TextInput
-                            style={styles.passwordInput}
-                            placeholder="Select bank"
-                            value=""
-                          />
-                           <Image source={require("../assets/arrow.png")}/>
-                        
-                        </View>
-
-                        <View style={styles.passwordContainer}>
-                          <TextInput
-                            style={styles.passwordInput}
-                            placeholder="Account number"
-                            value=""
-                          />
-                        
-                        </View>
-
-                         <TouchableOpacity style={styles.button}>
-                                  <Text style={styles.buttonText}>Continue</Text>
-                                </TouchableOpacity>
                 </View>
-          </SafeAreaView>
-    </ScrollView>
-    )
+
+                <View style={styles.warningBox}>
+                  <Image 
+                    source={require("../assets/Danger.png")}
+                    style={{width:14,height:14}}
+                  />
+                  <View style={{flex:1,paddingRight:15}}>
+                    <Text style={styles.warningText}>
+                      You cannot withdraw your interest since your durations has not been completed
+                    </Text>
+                  </View>
+                </View>
+
+              </View>
+
+              <View>
+
+                <View style={{flexDirection: "row", justifyContent: "space-between",marginBottom:5,paddingHorizontal:6}}>
+                    <Text style={{fontSize:12,color:'#292B2D'}}>Early Withdrawal Fee</Text>
+                    <Text style={{fontSize:12,color:'#FD3C4A'}}>- N10.00</Text>
+                </View>
+
+                <View style={styles.passwordContainer}>
+                  <TextInput
+                    style={styles.passwordInput}
+                    placeholder="Amount"
+                    placeholderTextColor={'#6C727F'}
+                  />
+                  <Image source={require("../assets/NairaImg.png")}/>
+                </View>
+
+                <View style={{flexDirection: "row",height:25,justifyContent: "flex-end", gap: 20,marginTop:10}}>
+                    <TouchableOpacity style={styles.selector}>
+                        <Text style={{fontSize:14,color: "#442CF5"}}>All</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.selector}>
+                        <Text style={{fontSize:14,color: "#442CF5"}}>50%</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{gap:10,marginTop:10}}>
+                  <TouchableOpacity style={styles.select_div}>
+                    <Text style={{fontSize:14,color:'#6C727F'}}>Select bank</Text>
+                    <Image source={require("../assets/arrow.png")}/>
+                  </TouchableOpacity>
+
+                  <View style={styles.passwordContainer}>
+                    <TextInput
+                      style={styles.passwordInput}
+                      placeholder="Account number"
+                      placeholderTextColor={'#6C727F'}
+                    />
+                  </View>
+                </View>
+
+              </View>
+
+            </View>
+          </ScrollView>
+
+          <View style={{paddingHorizontal:15, paddingBottom:20,paddingTop:10}}>
+            {
+              activeButton?
+              <CustomButton
+                backgroundColor={'#2C14DD'}
+                title={'Continue'}
+              /> 
+              :
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Continue</Text>
+              </View>
+            }
+          </View>
+
+        </SafeAreaView>
+      </View>
+  )
 }
 
 export default FinalWithdrawal
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#f3f3ff",
-    },
     header: {
       flexDirection: "row",
       alignItems: "flex-end",
@@ -134,26 +152,38 @@ const styles = StyleSheet.create({
     },
     card: {
       backgroundColor: "white",
-      borderRadius: 10,
-      padding: 20,
+      borderRadius: 16,
+      paddingTop: 20,
+      paddingBottom:10,
+      paddingHorizontal:10,
       alignItems: "center",
-      marginTop: 15
+      marginTop: 15,
+      gap:15
     },
     lockIcon: {
       backgroundColor: "#442CF5",
-      padding: 15,
-      borderRadius: 50,
-      marginBottom: 10,
+      height:60,
+      width:60,
+      borderRadius: '50%',
+      justifyContent:'center',
+      alignItems:'center'
     },
     totalInterest: {
-      fontSize: 16,
-      color: "#777",
+      fontSize: 14,
+      color: "#6C727F",
     },
     amount: {
-      fontSize: 28,
-      fontWeight: "bold",
-      color: "#000",
-      marginVertical: 5,
+      fontSize: 24,
+      fontWeight: 700,
+      color: "#131313",
+    },
+    info: {
+      flex:1,
+      backgroundColor: "#F5F7FF", 
+      flexDirection: "row", 
+      padding: 10, 
+      borderRadius: 10, 
+      gap: 10
     },
     details: {
       width: "100%",
@@ -168,61 +198,80 @@ const styles = StyleSheet.create({
     },
     warningBox: {
       backgroundColor: "#FFFAEC",
-      padding: 10,
-      marginTop: 20,
-      borderRadius: 5,
-    //   width: "100%",
+      paddingHorizontal: 6,
+      paddingVertical:4,
+      borderRadius: 10,
+      width: "100%",
       flexDirection: "row",
       alignItems: "center",
       gap: 10
     },
     warningText: {
-      fontSize: 12,
+      fontSize: 10,
+      color:'#292B2D',
+      lineHeight:18
     },
     button: {
-        backgroundColor: "#4a3aff",
-        padding: 15,
-        borderRadius: 15,
-        alignItems: "center",
-        marginTop: 37
+      height:50,
+      backgroundColor: "#b9b3f5",
+      borderRadius: 16,
+      alignItems: "center",
+      justifyContent:'center'
     },
     buttonText: {
-      color: "white",
-      fontSize: 16,
-      fontWeight: "bold",
+      color: "#FCFCFC",
+      fontSize: 14,
+      fontWeight: 600,
     },
 
     capital: {
-        color: "#6C727F",
+      color: "#6C727F",
+      fontSize:12
     },
-
     number: {
-        fontSize: 18,
-        fontWeight: "bold"
+      fontSize: 14,
+      fontWeight: 600,
+      color:'#131313'
     },
 
     lock: {
-        backgroundColor: "#442CF5",
-        padding: 15,
-        borderRadius: 50,
-      },
-
-      locktwo: {
-        backgroundColor: "#00A86B",
-        padding: 15,
-        borderRadius: 50,
+      width:30,
+      height:30,
+      backgroundColor: "#442CF5",
+      borderRadius: '50%',
+      justifyContent:'center',
+      alignItems:'center'
       },
       passwordContainer: {
+        height:50,
         flexDirection: "row",
         alignItems: "center",
         backgroundColor: "white",
-        padding: 10,
-        borderRadius: 18,
-        marginBottom: 10,
-        marginTop: 15
+        paddingHorizontal: 12,
+        borderRadius: 16,
+        gap:10
       },
       passwordInput: {
         flex: 1,
+        // backgroundColor:'red'
       },
+      selector: {
+        backgroundColor: "white", 
+        borderColor: "#442CF5", 
+        borderWidth: 1, 
+        borderRadius: 10, 
+        paddingHorizontal:20,
+        height:'100%',
+        justifyContent:'center'
+      },
+      select_div: {
+        height:50,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        backgroundColor:'white',
+        borderRadius:16,
+        paddingHorizontal:15
+      }
   });
   
