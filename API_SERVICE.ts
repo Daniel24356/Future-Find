9056a0c151b5feed7e8e24859fe15c2fda38c5ae
Korede Anunusor <koredeanunusor@gmai.com>
2025-03-22 14:18:35 +0100

    contribution functionality API

---
+++ API_SERVICE.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://192.168.7.174:5000/api/v1/contribution"; // Replace with actual backend URL
+
+export const inviteUsersToContribution = async (contributionId: string, userIds: string[]) => {
+  return await axios.post(`${API_BASE_URL}/contribution/invite`, { contributionId, userIds });
+};
+
+export const verifyIdentityAndJoin = async (userId: string, contributionId: string, verificationData: any) => {
+  return await axios.post(`${API_BASE_URL}/contribution/join`, { userId, contributionId, verificationData });
+};
+
+export const agreeToPaymentTerms = async (userId: string, contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/agree-to-terms`, { userId, contributionId });
+};
+
+export const assignContributionTurns = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/assign-turns`, { contributionId });
+};
+
+export const startContributionCycle = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/start-cycle`, { contributionId });
+};
+
+export const holdFundsInEscrow = async (contributionId: string, amount: number) => {
+  return await axios.post(`${API_BASE_URL}/contribution/hold-funds`, { contributionId, amount });
+};
+
+export const enforceTrustBuildingPeriod = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/enforce-trust`, { contributionId });
+};
+
+export const processPayouts = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/process-payouts`, { contributionId });
+};
+
+export const splitPayoutForSecurity = async (contributionId: string, payoutAmount: number) => {
+  return await axios.post(`${API_BASE_URL}/contribution/split-payout`, { contributionId, payoutAmount });
+};
+
+export const penalizeLatePayers = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/penalize-late`, { contributionId });
+};
+
+export const reassignForfeitedSpots = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/reassign-spots`, { contributionId });
+};
+
+export const finalizeContributionCycle = async (contributionId: string) => {
+  return await axios.post(`${API_BASE_URL}/contribution/finalize-cycle`, { contributionId });
+};
